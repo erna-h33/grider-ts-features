6012ebd4a816eccb42512c32ffeb2d23b253d8f8
erna-h33 <erna.halim33@gmail.com>
2026-05-30 06:37:53 +1000

    first commit

---
+++ features/annotations/variables.ts
@@ -0,0 +1,35 @@
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
+
+// arrays
+let colors: string[] = ['red', 'blue', 'green'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+// classes
+class Car {}
+let car: Car = new Car();
+
+// object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
+
+// function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// When to use annotations
+// 1) Fucntion that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates); // output {x: 10, y: 20}
---
+++ fetchjson/index.js
@@ -0,0 +1,10 @@
+"use strict";
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+    console.log(data);
+})
+    .catch(error => {
+    console.error(error);
+});
