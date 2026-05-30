c215ef6c4e382646717297a2c8fc61cad0493630
erna-h33 <erna.halim33@gmail.com>
2026-05-31 00:02:02 +1000

    more arrays

--- features/arrays.ts
+++ features/arrays.ts
@@ -2,3 +2,21 @@ const carMakers = ['ford', 'toyota', 'chevy'];
 const dates = [new Date(), new Date()];
 
 const carsByMake = [['f150'], ['corolla'], ['camaro']];
+
+// Why we car about arrays
+// 1) Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// 2) Prevent incompatible values
+carMakers.push(100);
+
+// 3) Help with map
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
+
+// Flexible types
+const importantDates: (Date | string)[] = [];
+importantDates.push('2030-10-10');
+importantDates.push(new Date());
